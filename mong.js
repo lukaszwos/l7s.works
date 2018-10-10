b9fd82ad3ffceb39328ea09c802bf1ae0bf8e7e7
Łukasz Woś <lukasz.wos@gmail.com>
2018-10-10 17:43:55 +0200

    zobaczymy co sie stanie

--- mong.js
+++ mong.js
@@ -1,17 +1,8 @@
-const db = require('monk')('localhost/mydb')
-// or
-// const db = require('monk')('user:pass@localhost:port/mydb')
- 
-const users = db.get('users')
- 
-users.index('name last')
-users.insert({ name: 'Tobi', bigdata: {} })
-users.find({ name: 'Loki' }, '-bigdata').then(function () {
-  // exclude bigdata field
-})
-users.find({}, {sort: {name: 1}}).then(function () {
-  // sorted by name field
-})
-users.remove({ name: 'Loki' })
- 
-db.close()
+const monk = require('monk');
+
+const db = monk('localhost/meower');
+const mews = db.get('mews');
+
+mews.insert({'mew': "Miau",
+			'costm': 'else'}).then(created => console.log(created));
+			
